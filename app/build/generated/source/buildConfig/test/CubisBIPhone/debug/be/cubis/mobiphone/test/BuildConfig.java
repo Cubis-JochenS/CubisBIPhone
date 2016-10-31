b238fdc44b0de890ac09c315e27d5b1efb12dc8e
Jochen Stroobants <Jochen@Jochens-MacBook-Pro.local>
2016-10-31 17:20:30 +0100

    Initial commit

---
+++ app/build/generated/source/buildConfig/test/CubisBIPhone/debug/be/cubis/mobiphone/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package be.cubis.mobiphone.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "be.cubis.mobiphone.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "CubisBIPhone";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
